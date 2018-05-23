77f55938f39a190d19b372ed76a5d5a7db3b37eb
Graham Wood <grahamwood13@gmail.com>
2018-05-23 11:31:03 -0400

    Began Intent Setup Ref #1 Ref #7

--- app/src/main/java/edu/hanover/cs323_grahamkennyproject/MainActivity.java
+++ app/src/main/java/edu/hanover/cs323_grahamkennyproject/MainActivity.java
@@ -2,6 +2,7 @@ package edu.hanover.cs323_grahamkennyproject;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.view.View;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -10,5 +11,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickSignIn (View view) {
+        Intent intent = new Intent(MainActivity.this, SignInActivity.class);
     }
 }
