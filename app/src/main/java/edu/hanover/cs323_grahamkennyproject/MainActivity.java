09407c1336dc558f26523ca354e30f906ad50885
Graham Wood <grahamwood13@gmail.com>
2018-05-23 14:13:09 -0400

    main_menu.xml added to provide actions on the ActionBar One Action, settings, has been completed, with intent Ref #3 Ref #4

--- app/src/main/java/edu/hanover/cs323_grahamkennyproject/MainActivity.java
+++ app/src/main/java/edu/hanover/cs323_grahamkennyproject/MainActivity.java
@@ -2,6 +2,8 @@ package edu.hanover.cs323_grahamkennyproject;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.os.Bundle;
 
@@ -16,5 +18,23 @@ public class MainActivity extends Activity {
     public void onClickSignIn (View view) {
         Intent intent = new Intent(MainActivity.this, SignInActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.Settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
---
+++ app/src/main/java/edu/hanover/cs323_grahamkennyproject/SettingsActivity.java
@@ -0,0 +1,14 @@
+package edu.hanover.cs323_grahamkennyproject;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
