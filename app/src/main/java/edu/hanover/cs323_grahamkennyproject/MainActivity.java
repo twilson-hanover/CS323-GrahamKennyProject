cb223779298dd0f2e3a6a020e2b84823b1f08c5b
Graham Wood <grahamwood13@gmail.com>
2018-05-23 11:21:35 -0400

    New Activity with an edit text Not currently connected to MainActivity #4

--- app/src/main/java/edu/hanover/cs323_grahamkennyproject/MainActivity.java
+++ app/src/main/java/edu/hanover/cs323_grahamkennyproject/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.hanover.cs323_grahamkennyproject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
