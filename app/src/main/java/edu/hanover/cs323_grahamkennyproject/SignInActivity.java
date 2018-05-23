c213b0ae086f097a65aa8f3d44b7766fe7575772
Graham Wood <grahamwood13@gmail.com>
2018-05-23 11:24:37 -0400

    Actually added new Activity and EditText this time Ref #4

---
+++ app/src/main/java/edu/hanover/cs323_grahamkennyproject/SignInActivity.java
@@ -0,0 +1,13 @@
+package edu.hanover.cs323_grahamkennyproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SignInActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
