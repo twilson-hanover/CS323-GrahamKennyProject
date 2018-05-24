5b1ecc90b57aaf9e558ff4cf4450971bf393e468
Kenny Jarnagin <jarnagink19@hanover.edu>
2018-05-24 13:21:49 -0400

    Implement Toast using ListView and subclass ItemClickListener close #2

--- app/src/main/java/edu/hanover/cs323_grahamkennyproject/SettingsActivity.java
+++ app/src/main/java/edu/hanover/cs323_grahamkennyproject/SettingsActivity.java
@@ -3,8 +3,11 @@ package edu.hanover.cs323_grahamkennyproject;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class SettingsActivity extends Activity {
 
@@ -19,5 +22,15 @@ public class SettingsActivity extends Activity {
         settingsList = (ListView) findViewById(R.id.SettingsList);
         settingsList.setAdapter(new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, titles));
+
+        settingsList.setOnItemClickListener(new ItemClickListener());
+    }
+
+    private class ItemClickListener implements ListView.OnItemClickListener{
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Toast toast = Toast.makeText(SettingsActivity.this, "Not yet implemented", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
