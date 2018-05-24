69ed0de5f0b4e70d7b541b963d0885f15baabaf9
Kenny Jarnagin <jarnagink19@hanover.edu>
2018-05-24 13:08:42 -0400

    Create ListView for Settings activity close #4

--- app/src/main/java/edu/hanover/cs323_grahamkennyproject/SettingsActivity.java
+++ app/src/main/java/edu/hanover/cs323_grahamkennyproject/SettingsActivity.java
@@ -3,12 +3,21 @@ package edu.hanover.cs323_grahamkennyproject;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class SettingsActivity extends Activity {
+
+    private String[] titles;
+    private ListView settingsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        titles = getResources().getStringArray(R.array.titles);
+        settingsList = (ListView) findViewById(R.id.SettingsList);
+        settingsList.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, titles));
     }
 }
