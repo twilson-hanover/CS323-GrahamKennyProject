6b8fe27e1da35fd24f8bc3c9d5cd822804fb2ee9
Kenny Jarnagin <jarnagink19@hanover.edu>
2018-05-24 14:57:51 -0400

    WIP load saved instance state

--- app/src/main/java/edu/hanover/cs323_grahamkennyproject/SignInActivity.java
+++ app/src/main/java/edu/hanover/cs323_grahamkennyproject/SignInActivity.java
@@ -8,17 +8,21 @@ import android.widget.TextView;
 
 public class SignInActivity extends Activity {
 
+    TextView tv = (TextView) findViewById(R.id.Patrons);
+    String origtv = tv.getText().toString();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        if (savedInstanceState != null) {
+            origtv = savedInstanceState.getString("origtv");
+        }
     }
 
     //Get the text from the EditText box and add it to the TextView
     public void onClickSignIn (View view) {
         EditText et = (EditText) findViewById(R.id.SignIn);
-        TextView tv = (TextView) findViewById(R.id.Patrons);
-        String origtv = tv.getText().toString();
         String newtv = origtv + et.getText().toString() + "\n";
         tv.setText(newtv);
         et.setText("");
@@ -26,7 +30,11 @@ public class SignInActivity extends Activity {
 
     //Clear the textview
     public void onClickClear (View view) {
-        TextView tv = (TextView) findViewById(R.id.Patrons);
         tv.setText("");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putString("origtv",origtv);
     }
 }
