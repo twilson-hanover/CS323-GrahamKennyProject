4217a31ca4efe02c6f2e654dbd301cde3ab1b745
kenjarna <jarnagink19@hanover.edu>
2018-05-26 15:59:34 -0400

    Reference the imageview to store when scan button clicked Ref #1

--- app/src/main/java/edu/hanover/cs323_grahamkennyproject/MainActivity.java
+++ app/src/main/java/edu/hanover/cs323_grahamkennyproject/MainActivity.java
@@ -4,8 +4,21 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
 
 public class MainActivity extends Activity {
 
@@ -13,6 +26,17 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.scan);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                ImageView IV = (ImageView) findViewById(R.id.imgview);
+                Bitmap myBitmap = BitmapFactory.decodeResource(
+                        getApplicationContext().getResources(),
+                        R.drawable.puppy);
+                IV.setImageBitmap(myBitmap);
+            }
+        });
     }
 
     //Populates the ActionBar with icons as needed
