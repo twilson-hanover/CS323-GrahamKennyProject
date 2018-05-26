b235a6cb8178626a0c7aa255027b8b23f075ee86
kenjarna <jarnagink19@hanover.edu>
2018-05-26 15:44:21 -0400

    Setup for processing a barcode scan Ref #1

---
+++ app/src/main/java/edu/hanover/cs323_grahamkennyproject/BarcodeActivity.java
@@ -0,0 +1,13 @@
+package edu.hanover.cs323_grahamkennyproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BarcodeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barcode);
+    }
+}
